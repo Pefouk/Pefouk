6918e385aaaaaa74a5a426cf187772698827657b
Appriou Jérémy <jeremy.appriou@gmail.com>
2021-06-21 15:56:37 +0200

    ajouts de classe flex pour le css, edit de la page pour séparer les bouttons visuellement

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,22 @@
     <h1 class="primary row">Jérémy Appriou</h1>
     <h2 class="row">Développeur web</h2>
     <div class="row">
-      <p class="btn-s btn-primary">smol btn</p>
-      <p class="btn btn-primary">normal btn</p>
-      <p class="btn-l btn-primary">big btn</p>
-      <p class="btn-xl btn-primary">very big btn</p>
+      <div class="flex f-row f-spa-eve w-100">
+        <p class="btn-s btn-primary">smol btn</p>
+        <p class="btn btn-primary">normal btn</p>
+        <p class="btn-l btn-primary">big btn</p>
+        <p class="btn-xl btn-primary">very big btn</p>
+      </div>
     </div>
     <div class="row">
-      <p class="btn btn-primary">btn primary</p>
-      <p class="btn btn-secondary">btn secondary</p>
-      <p class="btn btn-danger">btn danger</p>
-      <p class="btn btn-light">btn light</p>
-      <p class="btn btn-dark">btn dark</p>
-      <p class="btn btn-background">btn background</p>
+      <div class="flex f-row f-spa-eve w-100">
+        <p class="btn btn-primary">btn primary</p>
+        <p class="btn btn-secondary">btn secondary</p>
+        <p class="btn btn-danger">btn danger</p>
+        <p class="btn btn-light">btn light</p>
+        <p class="btn btn-dark">btn dark</p>
+        <p class="btn btn-background">btn background</p>
+      </div>
     </div>
   </div>
 </template>
