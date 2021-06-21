087c1de9aaf47be2ff8d35fddf05c68a81ef23a4
Appriou Jérémy <jeremy.appriou@gmail.com>
2021-06-21 12:32:23 +0200

    Bon include du scss

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
 <h1>Jérémy Appriou</h1>
+<h2>Développeur web et web mobile</h2>
+<div>
+  <p class="primary">Primaire</p><p class="secondary">Secondaire</p><p class="text">Text normal</p><p class="danger">Danger</p>
+</div>
 </template>
 
 <script lang="ts">
